e7067207b07e08c5cbadbb7d7acbe638d5f1b8fd
unknown <patrickr.89sc@gmail.com>
2021-07-21 12:45:17 +0200

    start script added, port changed

--- app.js
+++ app.js
@@ -112,6 +112,8 @@ app.use((err, req, res, next) => {
   //   return res.redirect("/home");
 });
 
-app.listen(3000, () => {
-  console.log("Server active on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server active on port ${port}`);
 });
